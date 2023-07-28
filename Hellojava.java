6dd7a126d8fb67dd7df01332eb17a8cd49118146
Dasun Bandara <dasun8517@gmail.com>
2023-07-28 12:59:32 +0530

    4th commit

--- Hellojava.java
+++ Hellojava.java
@@ -2,7 +2,5 @@ public class Hellojava{
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Java");
-        int a=2;
-        System.out.println(a);
     }
 }
